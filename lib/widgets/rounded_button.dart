63f45bbec0635957862fd3e282e72f8874fcc548
zack <shiqizhenyes@gmail.com>
2020-06-01 11:52:51 +0800

    optimize ui detail on iphone
    optimize ui detail on iphone

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class RoundedButton extends StatelessWidget {
+
   final String text;
   final Function press;
   final double verticalPadding;
+  final double horizontalPadding;
   final double fontSize;
-  const RoundedButton({
+
+  RoundedButton({
     Key key,
     this.text,
     this.press,
     this.verticalPadding = 16,
-    this.fontSize = 16,
+    this.horizontalPadding = 30,
+    this.fontSize = 16, 
   }) : super(key: key);
 
   @override
@@ -18,10 +22,11 @@ class RoundedButton extends StatelessWidget {
     return GestureDetector(
       onTap: press,
       child: Container(
+        width: double.infinity,
         alignment: Alignment.center,
         margin: EdgeInsets.symmetric(vertical: 16),
         padding:
-            EdgeInsets.symmetric(vertical: verticalPadding, horizontal: 30),
+            EdgeInsets.symmetric(vertical: verticalPadding, horizontal: horizontalPadding),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(30),
